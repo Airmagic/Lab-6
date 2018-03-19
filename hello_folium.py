9bf7023d8ce39c4faeda917df4509c4a6304a0c5
Eric Holmboe <babaphat@yahoo.com>
2018-03-19 15:22:41 -0500

    added comments

--- hello_folium.py
+++ hello_folium.py
@@ -1,7 +1,11 @@
+# This is a pip install
 import folium
 
+# this is a map location
 map_mn = folium.Map(location = [45, -93.2], zoom_start = 13)
 
+# adding a marker to the map for MCTC
 folium.Marker([44.9729, -93.2831], popup='MCTC').add_to(map_mn)
 
+# saving this to the map
 map_mn.save("map.html")
